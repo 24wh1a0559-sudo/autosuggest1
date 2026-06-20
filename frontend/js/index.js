c669c07c503b5b40475870c95bb31e5ba29bf0f1
thangallapally_Varshini <24wh1a0559@bvrithyderabad.edu.in>
2026-06-20 08:58:10 +0530

    toggle change

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,21 @@
 //alert("Hello from js !");
 
 //console.log("hello from js");
+const userImg = document.getElementById("userImg");
+const userName = document.getElementById("userName");
+const gender = document.getElementById("gender");
+const toggleBtn = document.getElementById("toggleBtn");
+
+toggleBtn.addEventListener("click", function () {
+
+    if (gender.textContent === "Male") {
+        userImg.src = "../images/jane.png";
+        userName.textContent = "Jane Doe";
+        gender.textContent = "Female";
+    } else {
+        userImg.src = "../images/john.png";
+        userName.textContent = "John Doe";
+        gender.textContent = "Male";
+    }
+
+});
